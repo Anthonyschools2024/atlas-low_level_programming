b6c06580321b46f5006f1c14ef2f592e09ae5427
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-20 17:54:26 +0000

    updated 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints a message
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-    /* Print a message */
+    /* Print the message */
     puts("Programming is like building a multilingual puzzle");
 
     return (0);
 }
+
 
