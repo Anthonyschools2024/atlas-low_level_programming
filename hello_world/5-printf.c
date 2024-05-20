6cd95e6ab048d84641fc391f6eaf988c93e06c9b
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-20 18:34:11 +0000

    hello printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a piece of art with proper grammar
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    printf("In the vast expanse of the digital canvas,\n\
+            pixels dance in a symphony of colors.\n\
+            Each stroke, a testament to creativity.\n\
+            A masterpiece unfolds, pixel by pixel.\n\
+            Art, born from the binary ballet.\n\n");
+
+    return 0;
+}
