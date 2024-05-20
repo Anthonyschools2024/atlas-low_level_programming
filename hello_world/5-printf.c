d4bdd98fc354c4ac5d60a233bb760bf8497bdcf3
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-20 18:40:34 +0000

    updated 5 again

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Will print an exact line using printf
+ * main - Prints a simple phrase using the main function
  *
  * Return: 0
  */
-
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art, ");
-
-	return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
