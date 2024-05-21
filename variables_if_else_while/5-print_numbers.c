8e22a5d04c64d126c7c0a7b8720f65f89bb2c9ad
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-21 02:23:29 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single-digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    /* Loop through single-digit numbers from 0 to 9 */
+    for (i = 0; i <= 9; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return (0);
+}
