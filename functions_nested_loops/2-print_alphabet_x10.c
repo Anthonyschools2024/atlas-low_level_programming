01d3f3c80268247e829ba9854d70ca413ea0bd0f
Anthoyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-24 19:33:45 +0000

    alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print-alphabet_10times - will print it 10 times
+ *
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+
+  int i;
+  char letter;
+
+  while (i=1; <=10; i++)
+    {
+      while (letter = 'a'; letter <= 'z'; i++)
+	_putchar (letter);
+      _putchar('\n')
+
+	}
+
+}
