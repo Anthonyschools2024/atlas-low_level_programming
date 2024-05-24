cc632de12f208ab3cadee930a0162320be33a23f
Anthoyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-24 18:03:06 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main"
+
+/**
+ * main - prints "_putchar \n" without libraries
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+  char str[] = "_putchar";
+  int i;
+
+  for (i = 0; i < 8; i++)
+
+    {
+
+      _putchar(str[i]);
+
+    }
+
+  _putchar('\n');
+
+  Return (0);
+
+}
+
+  
