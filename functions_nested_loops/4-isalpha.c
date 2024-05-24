e0229afbe87ac2670fcd2194ce1411544c514114
Anthoyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-24 20:24:21 +0000

    had to updaten code in file 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,35 @@
 #include "main.h"
+#include <stdio.h>
+#include <ctype.h>
 
+/**                                                                                                                                                                                                                            
+ * is_alphabetic - Checks if a character is alphabetic.                                                                                                                                                                        
+ * @c: Character to check.                                                                                                                                                                                                     
+ *                                                                                                                                                                                                                             
+ * Return: 1 if the character is alphabetic, 0 otherwise.                                                                                                                                                                      
+ */
+int is_alphabetic(char c)
+{
+    return isalpha(c);
+}
+
+int main(void)
+{
+    char test_char;
+
+    /* Testing the function with various characters */
+    test_char = 'A';
+    printf("%d\n", is_alphabetic(test_char)); // Should print 1                                                                                                                                                                
+
+    test_char = 'a';
+    printf("%d\n", is_alphabetic(test_char)); // Should print 1                                                                                                                                                                
+
+    test_char = '0';
+    printf("%d\n", is_alphabetic(test_char)); // Should print 0                                                                                                                                                                
+
+    test_char = ' ';
+    printf("%d\n", is_alphabetic(test_char)); // Should print 0                                                                                                                                                                
+     
 /**
  * is_alphabetic - Checks if a character is alphabetic.
  * @c: Character to check.
@@ -27,6 +57,6 @@ int main(void)
 
     test_char = ' ';
     printf("%d\n", is_alphabetic(test_char)); // Should print 0
-
+}
     return 0;
 }
