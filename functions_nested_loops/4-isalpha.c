726cab7311ffcc1acb87a48aa79c44f1a51d0285
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-05-25 03:18:31 +0000

    abs.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - returns 1 if lowercase character
- * @c: first operand
+ * _isalpha - checks if an input is in the alphabet
+ * @c: first param
  *
- * Return: 1 if true, 0 if false
+ * Return: 1 if letter, 0 otherwise
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
-
 	return (0);
 }
