eb424276a6e6cdf81296f7cbcd2dc6ffc8c4d8cd
Anthonyschools2024 <Anthony.Kimble@atlasschools.com>
2024-05-27 19:29:30 +0000

    main.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10x 0-14 followed by \n
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}
