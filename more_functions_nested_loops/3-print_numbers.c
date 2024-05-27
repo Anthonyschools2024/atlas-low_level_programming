20872ed1b25a76128a176dab9725f2f963d1c8dc
Anthonyschools2024 <Anthony.Kimble@atlasschools.com>
2024-05-27 18:40:41 +0000

    had to update file 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_numbers - prints 0-9 and a \n
+ *
  * Return: void
  */
 
 void print_numbers(void)
 {
+	int i;
 
-  int number; 
-
-  for (number = 0; number < 10; number++)
-{
-
-  _putchar(number);
-}
-
-  _putchar('\n');
+	for (i = 0; i < 10; i++)
+		_putchar('0' + i);
+	_putchar('\n');
 }
