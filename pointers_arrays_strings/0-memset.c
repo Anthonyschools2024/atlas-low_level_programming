34e9a97178d5f9c9aea1e5d16f0dc2b1ee69ca87
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-06-11 21:10:16 -0500

    updated file 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: the byte of memory
- * Return: s
- */
-
+* _memset - sets the array to the provided input for n number of spaces
+* in the array
+* Description - for n number of spaces in array s it sets it equal to b
+* @s: the inputed array
+* @b: the given value being inputted
+* @n: the amount of spaces in array s being set to b
+* Return: Always 0 (success)
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	int count = 0;
+	unsigned int i;
 
-	for (; n > 0; count++)
+	for (i = 0; i < n; i++)
 	{
-		s[count] = b;
-		n--;
+		s[i] = b;
 	}
+
 	return (s);
 }
