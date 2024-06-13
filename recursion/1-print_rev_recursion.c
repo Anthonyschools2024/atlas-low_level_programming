c5250f230d22ba288ad6fe4a71f175d9af92c376
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-06-13 17:08:08 -0500

    updated file 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,32 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * Prototype declaration
- */
-void _print_rev_recursion(char *s);
+* _print_rev_recursion - prints a string in reverse
+* @s: string
+* Return: void
+*/
 
-/**
- * Prints a string in reverse recursively
- * @param s The string to print in reverse
- */
-void _print_rev_recursion(char *s) {
-    // Base case: if the string is empty, return
-    if (*s == '\0') {
-        return;
-    }
-
-    // Recursive call for the rest of the string
-    _print_rev_recursion(s + 1);
-
-    // Print the current character
-    printf("%c", *s);
-}
-
-int main() {
-    char str[] = "Hello, World!";
-    printf("Original: %s\n", str);
-    _print_rev_recursion(str);
-    printf("\nReversed: ");
-    return 0;
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 }
