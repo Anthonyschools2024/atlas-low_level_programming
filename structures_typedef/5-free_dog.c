baba0a0e3b01809187936583acb92b7e96c25ff5
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-06-22 14:57:11 -0500

    for a second time had to update file 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,21 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * free_dog - frees allocated memory of a dog struct
- * @d: dog struct
+ * free_dog - this will free the memory of the dogs.(structs)
+ * @d: pointer to the dog struct.
  *
- * Return: void
  */
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		;
-	else
+	if (d != NULL)
 	{
-		free((*d).name);
-		free((*d).owner);
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
