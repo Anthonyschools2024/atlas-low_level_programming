e72548733d719290d38873f243003fa3edff478b
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-07-08 22:40:25 -0500

    under the snow

---
+++ file_io/1-create_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: name of file being created
+ * @text_content: content of file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int descriptor; /* file descriptor */
+	int num_write; /* number of chars written */
+	int content_length = 0; /* length of text_content */
+	mode_t permissions = S_IRUSR | S_IWUSR; /* permissions for new file */
+
+	if (filename == NULL) /* if filename NULL */
+	{
+		return (-1); /* return -1 */
+	}
+	/* open file, create if it doesn't exist, readwrite, truncate */
+	descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, permissions);
+
+	if (descriptor == -1) /* if any part of that fails */
+	{
+		return (-1); /* return -1 */
+	}
+
+	if (text_content == NULL) /* if text_content is NULL */
+	{
+		close(descriptor); /* close file */
+		return (1); /* return 1 */
+	}
+
+	if (text_content != NULL) /* if text_content is not NULL */
+	{
+		while (text_content[content_length] != '\0') /* find length */
+		{
+			content_length++; /* increment counter */
+		}
+		/* write to file */
+		num_write = write(descriptor, text_content, content_length);
+
+		if (num_write == -1) /* if writing fails */
+		{
+			return (-1); /* return -1 */
+		}
+	}
+
+	close(descriptor); /* close file */
+	return (1); /* return 1 */
+}
