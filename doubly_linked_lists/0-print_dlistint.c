6cd9967e81102a68c584541b3174eb0f0b7dc898
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-07-11 16:22:38 -0500

    had to update file 0 to correct format

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_dlistint - prints all elements of a dlistint_t list
  * @h: head of doubly linked list
@@ -6,15 +7,17 @@
  * Return: number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h) {
-    const dlistint_t *current = h;
-    size_t count = 0;
-
-    while (current != NULL) {
-        printf("%d\n", current->n); // Print the value of the node
-        current = current->next; // Move to the next node
-        count++; // Increment the counter
+size_t print_dlistint(const dlistint_t *h) 
+{
+       int node_count = 0; /*node counter*/
+  
+      while (h != NULL) /* iterate through the list until null */
+{
+        printf("%d\n", h->n); /* Print the value of the node */
+        node_count++; /* increment counter by 1 */
+        h = h->next; /* move to next node in list */
     }
 
-    return count; // Return the total number of nodes
+    return (node_count); /*  Return the total number of nodes */
+
 }
