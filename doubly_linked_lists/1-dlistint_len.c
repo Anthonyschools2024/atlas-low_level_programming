1f281e6f9f842c63da6bbc02312d4b4a08d2edf4
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-07-15 17:22:10 -0500

    had to update file 1 for doubly linked list

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
-
 /**
- * dlistint_len - returns number of elements in a doubly linked list
- * @h: pointer to head of list
+ * dlistint_len - returns the number of elements in a dlistint_t list
+ * @h: header of list
  *
- * Return: number of elements in list
+ * Return: number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
- {
-      int node_count; /* count nodes */
-
-      while (h != NULL) /* iterate through until null */ 
 {
-        node_count++; /* increment counter by 1 */
-        h = h->next; /* move to the next node */
-    }
+	size_t size = 0;
+	const dlistint_t *temp = h;
 
-    return node_count; /* return the number of nodes */
+	while (temp != NULL)
+	{
+		size++;
+		temp = temp->next;
+	}
+	return (size);
 }
