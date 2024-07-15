03a303e0f2098519b95d94d3227ba3824ab1b34f
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-07-15 17:35:13 -0500

    had to update file 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * *get_dnodeint_at_index - returns nth node of doubly linked dlistint_t list
+ * @head: pointer to head node of list
+ * @index: index of node to return
+ *
+ * Return: address of nth node or NULL if failed
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current_node = head; /* init pointer to head */
+	unsigned int index_count = 0; /* init index counter, set to 0 */
+
+	while (current_node != NULL) /* traverse list until NULL */
+	{
+		if (index_count == index) /* if index is found */
+		{
+			return (current_node); /* return address of that node */
+		}
+
+		current_node = current_node->next; /* move to next node */
+		index_count++; /* increment index counter */
+	}
+
+	return (NULL); /* return NULL if index not found */
+}
