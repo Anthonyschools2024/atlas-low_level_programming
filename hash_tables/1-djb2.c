bbba8be30ac1b12ee17504a3da8de89dee9b0cbf
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-07-15 21:58:36 -0500

    djb2

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Calculates the hash value of a string using the djb2 algorithm
+ * @str: Pointer to the string to be hashed
+ *
+ * Return: The hash value of the string
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash = 5381;
+    int c;
+
+    /* Process every character in the string */
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+
